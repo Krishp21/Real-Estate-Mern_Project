c31549166a8f8b47ec09539149f237e1471de524
Krish Patel <patelkp797@gmail.com>
2024-07-12 11:08:25 +0530

    Functionality of Sign Up Page completed

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    proxy:{ //each time we make a request to /api, it will be redirected to http://localhost:3000
+      '/api':{
+        target:'http://localhost:3000', 
+        secure: false,
+      },
+      }, 
+  },
   plugins: [react()],
 })
