53debcbb55dcaa996053d48c81b1a1e096e546f0
Krish Patel <patelkp797@gmail.com>
2024-07-19 01:55:51 +0530

    Complete upload listing images functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,6 +1,73 @@
 import React from 'react'
+import { useState } from 'react'
+import { getStorage, getDownloadURL, ref, uploadBytesResumable, } from 'firebase/storage'
+import { app } from '../firebase'
+import { set } from 'mongoose'
 
-function CreateListing() {
+
+
+export default function CreateListing() {
+    const [files, setFiles] = useState([])
+    const [formData, setFormData] = useState({
+        imageUrls:[],
+    })
+    const [imageUploadError, setImageUploadError] = useState(false)
+    const [uploading, setUploading] = useState(false)
+    const handleImageSubmit = (e) => {
+      if(files.length > 0 && files.length  + formData.imageUrls.length < 7){ //if there are files and the total number of files is less than 7
+        setUploading(true); 
+        setImageUploadError(false); //remove any previous error
+        const promises = []; //upload more than one image, more than one asynchronous behavior
+        
+       for(let i=0;i<files.length;i++){ //based on the length of the files array
+        promises.push(storeImage(files[i])); //add new promise to the promises array
+       }
+        Promise.all(promises)
+        .then((urls) => {
+          setFormData({
+            ...formData,
+            imageUrls: formData.imageUrls.concat(urls),
+          });
+          setImageUploadError(false);
+          setUploading(false);
+        })
+        .catch((err) => {
+          setImageUploadError('An error occurred while uploading the images. Please try again (2 MB max per image)');
+          setUploading(false);
+        });
+    } else {
+      setImageUploadError('You can only upload up to 6 images');
+      setUploading(false);
+    }
+  };
+
+    const storeImage = async(file) => {
+        return new Promise((resolve, reject) => {
+            const storage = getStorage(app);//get the storage service
+            const fileName = new Date().getTime() + file.name; //unique file name
+            const storageRef = ref(storage, fileName);//create a reference to the file 
+            const uploadTask = uploadBytesResumable(storageRef, file);//upload the file
+            uploadTask.on('state_changed', 
+                (snapshot) => {
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100; //percentage of the file uploaded
+                    console.log('Upload is ' + progress + '% done')},
+                (error)=>
+                {
+                    reject(error)
+                },
+                ()=>{   //when the upload is successful
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                        console.log('File available at', downloadURL);
+                        resolve(downloadURL)
+                    })
+                }
+            )
+        })
+    }
+    const handleRemoveImage = (index) => {
+        setFormData(
+            {...formData, imageUrls: formData.imageUrls.filter((_, i) => i !== index)}) //filter out the image url at the index
+    }
   return (
     <main className='p-3 max-w-4xl mx-auto'>      
      <h1 className='text-3xl font-semibold text-center my-7'>
@@ -8,14 +75,11 @@ function CreateListing() {
       </h1>
       <form className='flex flex-col sm:flex-row gap-4'>
         <div className='flex flex-col gap-4 flex-1'>
-          <input type='text' placeholder='Name' className='border p-3 rounded-lg' id='name' maxLength='62' minLength='10' required
-          />
+          <input type='text' placeholder='Name' className='border p-3 rounded-lg' id='name' maxLength='62' minLength='10' required/>
           <textarea
-            type='text' placeholder='Description' className='border p-3 rounded-lg' id='description' required
-          />
+            type='text' placeholder='Description' className='border p-3 rounded-lg' id='description' required/>
           <input
-            type='text' placeholder='Address' className='border p-3 rounded-lg' id='address' required
-          />
+            type='text' placeholder='Address' className='border p-3 rounded-lg' id='address' required/>
           <div className='flex gap-6 flex-wrap'>
             <div className='flex gap-2'>
               <input type='checkbox' id='sale' className='w-5' />
@@ -72,15 +136,27 @@ function CreateListing() {
           <span className='font-normal text-gray-600 ml-2'>The first image will be the cover (max 6)</span>
           </p>
           <div className="flex gap-4">
-            <input className='p-3 border border-gray-300 rounded w-full' type="file" id='images' accept='image/*' multiple />
-            <button className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'>Upload</button>
+            <input onChange={(e)=>setFiles(e.target.files)} className='p-3 border border-gray-300 rounded w-full' type="file" id='images' accept='image/*' multiple />
+            <button type='button' disabled={uploading} onClick={handleImageSubmit} className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'>
+               {uploading ? 'Uploading...' : 'Upload'}
+                </button>
           </div>
+          <p className='text-orange-700 text-sm'>{imageUploadError && imageUploadError}</p>
+          {
+            formData.imageUrls.length > 0 && formData.imageUrls.map((url,index) =>(
+            <div key={url} className='flex justify-between p-3 border items-center'>
+                <img src={url} alt='listing' className='w-20 h-20 object-contain rounded-lg'/>
+                <button type="button" onClick={()=>handleRemoveImage(index)} className='p-3 text-red-700 rounded-lg uppercase hover:opacity-75'>Delete</button>
+                </div>
+            ))
+        }
+              
+          
         <button className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Create Listing</button>
         </div>
+        
         </form>
         </main>
       
   )
 }
-
-export default CreateListing
