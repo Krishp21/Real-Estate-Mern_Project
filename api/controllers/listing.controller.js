4f6b6f16673002a683c9b93cca62bf8c0a7af943
Krish Patel <patelkp797@gmail.com>
2024-07-25 23:20:23 +0530

    Create update listing API route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
-import Listing from '../models/listing.model.js';
+import Listing from '../models/listing.model.js'
+import {errorHandler} from '../utils/error.js'
 export const createListing = async (req, res, next) => {
 try {
     const listing= await Listing.create(req.body); //create a new listing   
@@ -24,4 +25,20 @@ export const deleteListing = async (req, res, next) => {
         
     }
 
+}
+
+export const updateListing = async (req, res, next) => {
+    const listing= await Listing.findById(req.params.id); //find the listing by id
+    if(!listing) {return next(errorHandler(404, 'Listing not found'));} //if listing not found, return error
+    if(req.user.id != listing.userRef){
+        return next(errorHandler(401, 'Unauthorized')); //if user is not the creator of the listing, return error
+    }
+
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(req.params.id, req.body, {new:true}); //update the listing
+        res.status(200).json(updatedListing); //return the updated listing
+    } catch (error) {
+        next(error);
+        
+    }
 }
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListing, deleteListing } from '../controllers/listing.controller.js';
+import { createListing, deleteListing, updateListing } from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 
@@ -7,5 +7,5 @@ const router = express.Router();
 
 router.post('/create',verifyToken ,createListing);//create a new listing
 router.delete('/delete/:id',verifyToken, deleteListing) //:id for dynamic route
-
+router.post('/update/:id',verifyToken, updateListing) //:id for dynamic route
 export default router;
