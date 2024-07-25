8569f9423a2e28bf4243513602ab6bcd05b22cc9
Krish Patel <patelkp797@gmail.com>
2024-07-25 22:51:12 +0530

    Complete delete user listing functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -8,3 +8,20 @@ try {
 }
 
 }
+
+export const deleteListing = async (req, res, next) => {
+    const listing= await Listing.findById(req.params.id); //find the listing by id
+    if(!listing) {return next(errorHandler(404, 'Listing not found'));} //if listing not found, return error
+
+    if(req.user.id != listing.userRef){
+        return next(errorHandler(401, 'Unauthorized')); //if user is not the creator of the listing, return error
+    }
+
+    try {
+        await Listing.findByIdAndDelete(req.params.id); //delete the listing
+    } catch (error) {
+        next(error);
+        
+    }
+
+}
