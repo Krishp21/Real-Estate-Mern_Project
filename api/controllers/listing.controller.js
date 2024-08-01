0e1a2bdbcc5549fe0331e09434cf330accae4cea
Krish Patel <patelkp797@gmail.com>
2024-08-01 18:48:59 +0530

    Create search API route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -22,6 +22,7 @@ export const deleteListing = async (req, res, next) => {
 
     try {
         await Listing.findByIdAndDelete(req.params.id); //delete the listing
+        res.status(200).json('Listing deleted'); //return message to the user
     } catch (error) {
         next(error);
         
@@ -54,4 +55,45 @@ try {
     next(error);
     
 }
+}
+
+export const getListings = async (req, res, next) => {
+    try {
+        const limit = parseInt(req.query.limit) || 9; //limit the number of listings to show
+        const startIndex = parseInt(req.query.startIndex) || 0; //start index of the listings
+        let offer = req.query.offer; //filter by offer
+
+        if (offer === undefined || offer === 'false'){
+            offer= {$in : [false, true]}; //if offer is not specified, show all listings
+            }
+        
+        let furnished = req.query.furnished; //filter by furnished
+
+        if (furnished === undefined || furnished === 'false'){
+            furnished= {$in : [false, true]}; //if furnished is not specified, show all listings
+            }
+
+        let parking = req.query.parking; //filter by parking
+
+        if (parking === undefined || parking === 'false'){
+            parking= {$in : [false, true]}; //if parking is not specified, show all listings
+            }
+        
+        let type = req.query.type; //filter by type
+        if(type === undefined || type === 'all'){
+            type= {$in : ['sale', 'rent']}; //if type is not specified, show all listings
+            }
+        
+
+        const searchTerm= req.query.searchTerm || ''; //search for listings
+        const sort = req.query.sort || 'createdAt'; //sort the listings
+        const order = req.query.order || 'desc'; //order of sorting ;descending by default
+
+        const listings = await Listing.find(
+            {name:{$regex: searchTerm, $options: 'i'}, offer, furnished, parking, type,}).sort({[sort]: order}).limit(limit).skip(startIndex) //regex is the default search functionality in mongodb and limit the number of listings to show
+        return res.status(200).json(listings); //return the listings    
+      
+        } catch (error) {
+        next(error);      
+    }
 }
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListing, deleteListing, updateListing, getListing } from '../controllers/listing.controller.js';
+import { createListing, deleteListing, updateListing, getListing, getListings } from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 
@@ -8,6 +8,7 @@ const router = express.Router();
 router.post('/create',verifyToken ,createListing);//create a new listing
 router.delete('/delete/:id',verifyToken, deleteListing) //:id for dynamic route
 router.post('/update/:id',verifyToken, updateListing) //:id for dynamic route
-router.get('/getListing/:id',verifyToken, getListing) //:id for dynamic route
+router.get('/getListing/:id', getListing) //:id for dynamic route
+router.get('/get',getListings)
 
 export default router;
