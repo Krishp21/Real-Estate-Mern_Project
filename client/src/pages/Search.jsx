4efcec4268f3fd5c491a2ef02e360623e475d5c3
Krish Patel <patelkp797@gmail.com>
2024-08-07 11:00:29 +0530

    Add show more listings functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ListingItem from '../components/Listingitem';
+import { set } from 'mongoose';
 
 export default function Search() {
     const navigate = useNavigate();
@@ -16,7 +17,7 @@ export default function Search() {
 
     const [loading, setLoading] = useState(false);
     const [listings, setListings] = useState([]);
-    console.log(listings);
+    const [showMore, setShowMore] = useState(false);
 
     useEffect(() => {
         const urlParams = new URLSearchParams(location.search);
@@ -50,9 +51,15 @@ export default function Search() {
 
         const fetchListings = async () => {
             setLoading(true);
+            setShowMore(false)
             const searchQuery = urlParams.toString();
             const res = await fetch(`/api/listing/get?${searchQuery}`);
             const data = await res.json();
+            if(data.length>8){
+                setShowMore(true);
+            } else{
+                setShowMore(false);
+            }
             setListings(data);
             setLoading(false);
         };
@@ -107,6 +114,20 @@ export default function Search() {
         const searchQuery = urlParams.toString(); // converting the url parameters to string
         navigate(`/search?${searchQuery}`);
     };
+
+    const onShowMoreClick = async()=>{
+        const numberOfListings= listings.length;
+        const startIndex= numberOfListings;
+        const urlParams = new URLSearchParams(location.search);
+        urlParams.set('startIndex', startIndex);
+        const searchQuery = urlParams.toString();
+        const res = await fetch(`/api/listing/get?${searchQuery}`);
+        const data = await res.json();
+        if(data.length<9){
+            setShowMore(false);
+        }
+        setListings([...listings, ...data]);
+    }
     return (
         <div className='flex flex-col md:flex-row'>
             <div className='p-7  border-b-2 md:border-r-2 md:min-h-screen'>
@@ -191,7 +212,14 @@ export default function Search() {
                         </p>
                     )}
 
-                    {!loading && listings && listings.map((listing) => <ListingItem key={listing._id} listing= {listing}/>)}
+                    {!loading && listings && listings.map((listing) => (<ListingItem key={listing._id} listing= {listing}/>))}
+
+                    {showMore && (
+                        <button onClick={onShowMoreClick}
+                        className='text-green-700 hover:underline p-7 text-center w-full'
+                        >
+                            Show More
+                        </button>)}
                 </div>
             </div>
         </div>
